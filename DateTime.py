0cffa2838c8ba257058c5f504c9106e24acfcb77
Vlad Dombr <v.dombrovsky@gmail.com>
2015-12-09 15:36:46 +0200

    Add Current Time

--- DateTime.py
+++ DateTime.py
@@ -2,3 +2,9 @@ from datetime import datetime
 
 now = datetime.now()
 print now
+print "Current month is %s" % now.month
+print "Current day is %s" % now.day
+print "Current year is %s" % now.year
+
+print "%s/%s/%s" % (now.year, now.month, now.day)
+print "Current time is %s:%s:%s" % (now.hour, now.minute, now.second)
