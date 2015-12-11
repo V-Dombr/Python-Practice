8d635626142cc486946de756bd056ad211501eca
Vlad Dombr <v.dombrovsky@gmail.com>
2015-12-11 11:34:11 +0200

    Pig Latin program

---
+++ Pig Latin.py
@@ -0,0 +1,10 @@
+original = raw_input("Enter a word:")
+if len(original) > 0 and original.isalpha():
+    print original
+    word = original.lower()
+    first = word[0]
+    new_word = word + first + 'ay'
+    new_word = new_word[1:len(new_word)]
+    print new_word
+else:
+    print "Empty"
