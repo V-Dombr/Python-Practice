4f93fd68f2691f678f0ca5d7b4e881ea54c0979d
Vlad Dombr <v.dombrovsky@gmail.com>
2016-01-13 17:12:43 +0200

    Jan 13 Solve the Last Problem Python Course

--- Jan_13_MaxMinNumbers.py
+++ Jan_13_MaxMinNumbers.py
@@ -1,3 +1,8 @@
+# I am proud about this one!
+
+lowest = None
+highest = None
+
 while True:
     num = raw_input("Enter your number:" )
 
@@ -10,3 +15,17 @@ while True:
         print ("Only integer numbers should be entered!")
         continue
 
+    if lowest is None:
+        lowest = num
+
+    if highest is None:
+        highest = num
+
+    if num > highest:
+        highest = num
+
+    if num < lowest:
+        lowest = num
+
+print ('The highest number is: '), highest
+print ('The lowest number is: '), lowest
