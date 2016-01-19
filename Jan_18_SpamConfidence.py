41f4caf44e92a6c699feb6e840d1b8e2cc98c2ae
Vlad Dombr <v.dombrovsky@gmail.com>
2016-01-19 17:03:24 +0200

    Jan 19 Coursera Exercise

--- Jan_18_SpamConfidence.py
+++ Jan_18_SpamConfidence.py
@@ -1,5 +1,13 @@
 file_name = raw_input("Enter file name: ")
-file_data = open(file_name)
+
+#Make sure that everything is OK with a file name
+try:
+    file_data = open(file_name)
+except:
+    print ("Something is wrong with the file name")
+    exit()
+
+print file_data
 
 spam_confidence_sum = 0
 count_line = 0
@@ -7,4 +15,14 @@ count_line = 0
 for line in file_data:
     if line.startswith("X-DSPAM-Confidence:"):
         count_line = count_line + 1
-        
+        line = line.rstrip()
+
+        spam_confidence_sum = spam_confidence_sum + float(line[20:26])
+
+print "Number of lines", count_line
+print "Spam sum", spam_confidence_sum
+print "The average spam confidence", spam_confidence_sum/count_line
+
+# Close file and verify if we do this correctly
+file_data.close()
+print file_data
