f8f9c30eb551dfe666bb7866af75770c7bb6b4ed
Vlad Dombr <v.dombrovsky@gmail.com>
2016-01-28 17:41:40 +0200

    Jan 28 Coursera End of the Day

--- Jan_28_Coursera_Count_Words.py
+++ Jan_28_Coursera_Count_Words.py
@@ -1,15 +1,23 @@
 counts = dict()
 # words = list()
 
+# Ask for a text to process
 print 'Enter a line of text: '
 line = raw_input('>>')
 
+# Split text into words
 words = line.lower().split()
 print "Words: ", words
 
 print "Counting..."
 
+# Count words
 for word in words:
     counts[word] = counts.get(word, 0) + 1
 
 print "Counts: ", counts
+
+# Get rid of words which appear only 1 time and print the rest
+for key in counts:
+    if counts[key] != 1:
+        print key, counts[key]
---
+++ Jan_28_Coursera_Dic.py
@@ -0,0 +1,9 @@
+dic = {'apple':2, 'banana':4, 'bread':23}
+
+print dic
+
+print list(dic)
+print dic.keys()
+print dic.values()
+# return tuples
+print dic.items()
